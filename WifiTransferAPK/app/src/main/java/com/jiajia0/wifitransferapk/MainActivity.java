330388b37f24a329cb66df2d32b3e03f60a34306
leafage <majialongcode@gmail.com>
2018-01-29 18:17:13 +0800

    Update

--- WifiTransferAPK/app/src/main/java/com/jiajia0/wifitransferapk/MainActivity.java
+++ WifiTransferAPK/app/src/main/java/com/jiajia0/wifitransferapk/MainActivity.java
@@ -2,12 +2,19 @@ package com.jiajia0.wifitransferapk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setSupportActionBar(mToolbar);
     }
 }
