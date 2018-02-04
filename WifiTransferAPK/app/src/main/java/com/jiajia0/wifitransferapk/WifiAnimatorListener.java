34307b4f05d315f6ec09136b0c31e017cde9fbd6
leafage <majialongcode@gmail.com>
2018-02-04 16:09:31 +0800

    Wifi开启的动画监听器

---
+++ WifiTransferAPK/app/src/main/java/com/jiajia0/wifitransferapk/WifiAnimatorListener.java
@@ -0,0 +1,30 @@
+package com.jiajia0.wifitransferapk;
+
+import android.animation.Animator;
+
+/**
+ * Created by Leafage on 2018/2/4 13:09.
+ * DESCRIPTION : Wifi开启的动画监听器。
+ */
+
+public class WifiAnimatorListener implements Animator.AnimatorListener {
+    @Override
+    public void onAnimationStart(Animator animator) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animator animator) {
+
+    }
+
+    @Override
+    public void onAnimationCancel(Animator animator) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animator animator) {
+
+    }
+}
