3b6bc0b4a3f361ea9b1bbe66bac72d430cd37436
leafage <majialongcode@gmail.com>
2018-02-06 22:24:38 +0800

    UPDATE

--- WifiTransferAPK/app/src/main/java/com/jiajia0/wifitransferapk/Constants.java
+++ WifiTransferAPK/app/src/main/java/com/jiajia0/wifitransferapk/Constants.java
@@ -1,5 +1,9 @@
 package com.jiajia0.wifitransferapk;
 
+import android.os.Environment;
+
+import java.io.File;
+
 /**
  * Created by Leafage on 2018/2/5 11:08.
  * DESCRIPTION : 常量
@@ -7,12 +11,14 @@ package com.jiajia0.wifitransferapk;
 
 public class Constants {
     public static final int HTTP_PORT = 12345;// 端口号
+    public static final String DIR_IN_SDCARD = "WifiTransferAPK";
+    public static final File DIR = new File(Environment.getExternalStorageDirectory() + File.separator + Constants.DIR_IN_SDCARD);
 
     // RxBus传递用的参数
     public static final class RxBusEventType {
         public static final String POPUP_MENU_DIALOG_SHOW_DISMISS = "POPUP MENU DIALOG SHOW DISMISS";
         public static final String WIFI_CONNECT_CHANGE_EVENT = "WIFI CONNECT CHANGE EVENT";
-        public static final String LOAD_BOOK_LIST = "LOAD APK LIST";
+        public static final String LOAD_APK_LIST = "LOAD APK LIST";
         public static final int MSG_DIALOG_DISMISS = 0;
     }
 }
